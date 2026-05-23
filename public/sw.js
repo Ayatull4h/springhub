4c63290969fefb359bbce0a0153822669dfd23ac
Ayatull4h <ayatu@users.noreply.github.com>
2026-05-23 20:32:21 +0700

    fix: sw skipWaiting + clean old caches on activate

--- public/sw.js
+++ public/sw.js
@@ -10,9 +10,18 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
     })
   );
 });
